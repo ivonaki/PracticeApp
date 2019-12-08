76aeb30c62f2157253244180aac785d249ec0c0d
ivonaki <ivonakizlin0@gmail.com>
2019-12-08 19:32:41 +0100

    using services for pushing data from a to b

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,20 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { from } from 'rxjs';
+import { EventEmitter } from '@angular/core';
+
 export class ShoppingListService {
-    
+    ingredientsChanged= new EventEmitter<Ingredient[]>();
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+      ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }  
+
+    addIgredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
